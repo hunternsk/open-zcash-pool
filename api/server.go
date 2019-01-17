105ecb94f9f3f932509163b9dde927a689a144e8
Jakub Kowalski <jakub.kowalski@nextapps.pl>
2019-01-17 15:28:17 +0100

    Fix wrong accounts path regex
    Remove login being lowercased

--- api/server.go
+++ api/server.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"sort"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -105,7 +104,7 @@ func (s *ApiServer) listen() {
 	r.HandleFunc("/api/stats", s.StatsIndex)
 	r.HandleFunc("/api/miners", s.MinersIndex)
 	r.HandleFunc("/api/blocks", s.BlocksIndex)
-	r.HandleFunc("/api/accounts/{login:0x[0-9a-fA-F]{40}}", s.AccountIndex)
+	r.HandleFunc("/api/accounts/{login:t[0-9a-zA-Z]{34}}", s.AccountIndex)
 	r.NotFoundHandler = http.HandlerFunc(notFound)
 	err := http.ListenAndServe(s.config.Listen, r)
 	if err != nil {
@@ -228,7 +227,7 @@ func (s *ApiServer) AccountIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Cache-Control", "no-cache")
 
-	login := strings.ToLower(mux.Vars(r)["login"])
+	login := mux.Vars(r)["login"]
 	s.minersMu.Lock()
 	defer s.minersMu.Unlock()
 
